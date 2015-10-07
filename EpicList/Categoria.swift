28d194993880efa656d626d96255579cd1c9b33f
Larissa Gonçalves <larissag@Larissas-MacBook.local>
2015-10-06 21:32:30 -0300

    gitignore and realm

---
+++ EpicList/Categoria.swift
@@ -0,0 +1,18 @@
+//
+//  Categoria.swift
+//  EpicList
+//
+//  Created by Larissa Gonçalves on 10/5/15.
+//  Copyright © 2015 Larissa. All rights reserved.
+//
+
+import RealmSwift
+
+class Categoria: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
