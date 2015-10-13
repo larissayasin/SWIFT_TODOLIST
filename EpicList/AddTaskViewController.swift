37e30b6cf32e5644843936adb0b93719c7ab8b9e
Larissa Gonçalves <larissag@Larissas-MacBook.local>
2015-10-13 20:00:10 -0300

    Add task

--- EpicList/AddTaskViewController.swift
+++ EpicList/AddTaskViewController.swift
@@ -7,10 +7,56 @@
 //
 
 import UIKit
+import RealmSwift
+
 class AddTaskViewController: UIViewController{
-   
+    
+    @IBOutlet weak var txtTitulo: UITextField!
+    @IBOutlet weak var txtDescricao: UITextField!
+    @IBOutlet weak var txtCategoria: UITextField!
+    @IBOutlet weak var txtData: UITextField!
+    @IBOutlet weak var imgAdd: UIImageView!
+    @IBOutlet weak var btnFeito: UIBarButtonItem!
+    
+    var realm: Realm!
+    var userSet: UserSet!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        realm = try! Realm()
+        userSet = UserSet()
+        //        let predicate = NSPredicate(format: "nroNivel = %@", userSet.getUserLevel())
+        //        let funcionalidade = realm.objects(Nivel).filter(predicate).first
+        if(userSet.getUserLevel() == 0){
+            imgAdd.hidden = true
+        }
     }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "feitosegue"
+        {
+            let tarefa = Task()
+            let predicate = NSPredicate(format: "descricao = %@", txtDescricao.text!)
+            let categorias = realm.objects(Categoria).filter(predicate)
+            
+            tarefa.titulo = txtTitulo.text!
+            tarefa.descricao = txtDescricao.text!
+            if(categorias.count == 0){
+                let categoria = Categoria()
+                categoria.descricao = txtCategoria.text!
+                categoria.isRemovivel = true
+                realm.write{
+                    self.realm.add(categoria)
+                }
+                tarefa.categoria = categoria
+            }else{
+                tarefa.categoria = categorias.first
+            }
+            realm.write{
+                self.realm.add(tarefa)
+            }
+            
+        }
+    }
+    
 }
