530ede300f8064e8379117d5eec9491a46425c81
Larissa Gonçalves <larissag@Larissas-MacBook.local>
2015-10-13 13:10:14 -0300

    layout changed -with error

--- EpicList/ViewController.swift
+++ EpicList/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableview: UITableView!
+    @IBOutlet weak var btSelecionar: UIButton!
+    @IBOutlet weak var txtCategoria: UITextField!
+    @IBOutlet weak var btAdicionar: UIBarButtonItem!
+    
+    var realm: Realm!
+    var tarefas: List<Task>
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        realm = try! Realm()
+        tableview.delegate = self
+        tableview.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tarefas.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+      return nil
+    }
+    func loadDataFromDB(){
+//        let tarefas = realm.objects(Task)
+        
+    }
+    
 }
 
